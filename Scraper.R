088ab8854300518156139027e0c05fc18bc37022
Alexandre <prevosta@github.com>
2014-11-19 15:50:48 -0500

    (NF) Scraping Teams and Games Data

--- Scraper.R
+++ Scraper.R
@@ -1,27 +1,27 @@
-scrapePlayers <- function(scheme, host, path, years = 1998:2015, pages = 1:50, 	viewnames = c("summary",
-												      "assists",
-												      "bios",
-												      "divisionScoring",
-												      "faceOffPercentage",
-												      "faceOffPercentageAll",
-												      "goals",
-												      "homeScoring",
-												      "penalties",
-												      "plusMinus",
-												      "points",
-												      "rtssPlayerStats",
-												      "roadScoring",
-												      "shooting",
-												      "shootingAll",
-												      "shootouts",
-												      "scoringLeaders",
-												      "timeOnIce") )
+scrapePlayers <- function(scheme, host, path, years = 1998:2015, pages = 1:50, viewnames = c("summary",
+											     "assists",
+											     "bios",
+											     "divisionScoring",
+											     "faceOffPercentage",
+											     "faceOffPercentageAll",
+											     "goals",
+											     "homeScoring",
+											     "penalties",
+											     "plusMinus",
+											     "points",
+											     "rtssPlayerStats",
+											     "roadScoring",
+											     "shooting",
+											     "shootingAll",
+											     "shootouts",
+											     "scoringLeaders",
+											     "timeOnIce") )
 {
 	values <- c("20152ALLSASALL", "", "player.bioFirstNameLastName", "")
 	names(values) <- c("fetchKey", "viewName", "sort", "pg")
 	
 	output <- vector("list", (length(viewnames) * length(years) ) )
-
+	
 	for(inxYear in years)
 	{
 		for(inxViewName in 1:length(viewnames))
@@ -50,43 +50,115 @@ scrapePlayers <- function(scheme, host, path, years = 1998:2015, pages = 1:50, 	
 	output
 }
 
-scrapeTeams <- function(scheme, host, path, years = 1998:2015, pages = 1:50, 	viewnames = c("summary",
-											    "assists",
-											    "bios",
-											    "divisionScoring",
-											    "faceOffPercentage",
-											    "faceOffPercentageAll",
-											    "goals",
-											    "homeScoring",
-											    "penalties",
-											    "plusMinus",
-											    "points",
-											    "rtssPlayerStats",
-											    "roadScoring",
-											    "shooting",
-											    "shootingAll",
-											    "shootouts",
-											    "scoringLeaders",
-											    "timeOnIce") )
+scrapeTeams <- function(scheme, host, path, years = 1998:2015, pages = 1:50, viewnames = c("summary",
+											   "goalsHomeRoad",
+											   "goalsByPeriod",
+											   "goalsFor",
+											   "goalsAgainst",
+											   "goalGames",
+											   "outshootingOutshotby",
+											   "overtimeRecords",
+											   "penalties",
+											   "powerPlay",
+											   "powerPlayTime",
+											   "penaltyKill",
+											   "penaltyKillTime",
+											   "plusMinusTeam",
+											   "realTimeStats",
+											   "recordWhenLeading",
+											   "scoreTrailFirst",
+											   "shootoutTeamSummary") )
 {
+	values <- c("20152ALLSAAAll", "", "team.teamName", "")
+	names(values) <- c("fetchKey", "viewName", "sort", "pg")
+	
 	output <- vector("list", (length(viewnames) * length(years) ) )
+	
+	for(inxYear in years)
+	{
+		for(inxViewName in 1:length(viewnames))
+		{
+			cat(sprintf("\n %d - %s \n", inxYear, viewnames[inxViewName]))
+			
+			lastFirstName <- ""
+			inxOutput <- ((inxYear-min(years))*length(viewnames)) + inxViewName
+			
+			for(inxPage in pages)
+			{
+				values["fetchKey"] = paste(as.character(inxYear), "2ALLSAAAll", sep = "")
+				values["viewName"] = as.character(viewnames[inxViewName])
+				values["pg"] = as.character(inxPage)
+				
+				query <- getQuery(values)
+				url <- getURL(scheme, host, path, query)
+				
+				output[[inxOutput]] <- scrape(url, output[[inxOutput]])
+				
+				cat(".")
+			}
+		}
+	}
+	
+	output
+}
+
+scrapeGames <- function(scheme, host, path, years = 1998:2015, pages = 1:50, viewnames = c("summary",
+											   "gameSummary",
+											   "gameByGameOvertimesAndShootouts",
+											   "teamPenaltiesByGame",
+											   "penaltyShots",
+											   "teamRTSSreports",
+											   "gameByGameShutouts") )
+{
+	values <- c("20152ALLSATAll", "", "team.teamName", "")
+	names(values) <- c("fetchKey", "viewName", "sort", "pg")
+	
+	output <- vector("list", (length(viewnames) * length(years) ) )
+	
+	for(inxYear in years)
+	{
+		for(inxViewName in 1:length(viewnames))
+		{
+			cat(sprintf("\n %d - %s \n", inxYear, viewnames[inxViewName]))
+			
+			lastFirstName <- ""
+			inxOutput <- ((inxYear-min(years))*length(viewnames)) + inxViewName
+			
+			for(inxPage in pages)
+			{
+				values["fetchKey"] = paste(as.character(inxYear), "2ALLSATAll", sep = "")
+				values["viewName"] = as.character(viewnames[inxViewName])
+				values["pg"] = as.character(inxPage)
+				
+				query <- getQuery(values)
+				url <- getURL(scheme, host, path, query)
+				
+				output[[inxOutput]] <- scrape(url, output[[inxOutput]])
+				
+				cat(".")
+			}
+		}
+	}
+	
+	output
 }
 
 scrape <- function(url, output)
 {
 	html_tables <- readHTMLTable(url)
 	data_table <- html_tables[[length(html_tables)]]
-	firstName <- as.character( data_table[1,2])
+	firstName <- as.character( paste(data_table[1,2], data_table[1,2]) )
+	lastFirstName <- ""
 	
 	if(firstName != lastFirstName)
 	{
-		if(is.null(output[[inxViewName]]))
+		if(is.null(output))
 		{
-			output[[inxViewName]] <- data_table[,2:ncol(data_table)]
+			output <- data_table[,2:ncol(data_table)]
 		}
 		else
 		{
-			output[[inxViewName]] <- rbind(output[[inxViewName]], data_table[,2:ncol(data_table)]) 	
+			output <- rbind(output, data_table[,2:ncol(data_table)]) 	
 		}
 		
 		lastFirstName <- firstName
--- main.R
+++ main.R
@@ -3,7 +3,12 @@ source("Scraper.R")
 
 scheme <- "http"
 host <- "www.nhl.com"
-path <- "ice/playerstats.htm"
 
+path <- "ice/playerstats.htm"
 players_Data <- scrapePlayers(scheme, host, path, years = 2015, page = 1:2)
+
+path <- "ice/teamstats.htm"
 teams_Data <- scrapeTeams(scheme, host, path, years = 2015, page = 1:2)
+
+path <- "ice/gamestats.htm"
+games_Data <- scrapeGames(scheme, host, path, years = 2015, page = 1:2)
