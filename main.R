0607eab56701611e752e206b9b4346a96c3bfcd6
Alexandre <prevosta@github.com>
2014-11-19 03:28:25 -0500

    (NF) Scrape the data.

--- main.R
+++ main.R
@@ -1,1 +1,39 @@
-asd-asd
+library(XML)
+source("QueryBuilder.R")
+
+output <- vector("list", getNbViewnames()) 
+
+for(inxViewName in 1:getNbViewnames())
+{
+	print(inxViewName)
+	lastFirstID <- 9999
+	
+	for(inxPage in 1:50)
+	{
+		url <- getURL(inxViewName, inxPage)
+		tables <- readHTMLTable(url)
+		data_table <- tables[[length(tables)]]
+		firstID <- as.numeric( as.character( data_table[1,1]) )
+		
+		if(firstID != lastFirstID)
+		{
+			if(is.null(output[[inxViewName]]))
+			{
+				output[[inxViewName]] <- data_table[,2:ncol(data_table)]
+			}
+			else
+			{
+				output[[inxViewName]] <- rbind(output[[inxViewName]], data_table[,2:ncol(data_table)]) 	
+			}
+			
+			lastFirstID <- firstID
+		}
+		else
+		{
+			break
+		}
+		print('.')
+	}
+}
+
+return( output )
